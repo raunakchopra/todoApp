c83077693dfc1c1e9d260c381a268b993c05dc0b
raunakchopra <raunakchopra@github.com>
2020-09-14 03:21:51 +0800

    Frontend prepared, divison left and styling the listItem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  Typography,
+  Button,
+  TextField,
+  ListItem,
+  ListItemText,
+  List,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const classes = useStyles();
+
+  const handleSubmit = () => {
+    setTodos([...todos, input]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Typography align="center" variant="h1">
+        TODO APP
+      </Typography>
+      <div className="inputContainer">
+        <TextField
+          classes={{
+            root: classes.root,
+          }}
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <Button
+          disabled={input === "" ? true : false}
+          variant="contained"
+          color="secondary"
+          classes={{
+            root: classes.button,
+          }}
+          onClick={handleSubmit}
         >
-          Learn React
-        </a>
-      </header>
+          ADD TODO
+        </Button>
+      </div>
+      <List>
+        {todos.map((todo) => (
+          <ListItem>
+            <ListItemText primary={todo} />
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
 }
 
 export default App;
+
+const useStyles = makeStyles({
+  root: {
+    background: "white",
+    borderRadius: 3,
+    border: 0,
+    color: "#1A202C",
+    height: 48,
+    padding: "0 30px",
+    marginRight: "15px",
+  },
+  label: {
+    textTransform: "capitalize",
+  },
+  button: {
+    height: 48,
+  },
+  listItemText: {
+    fontSize: 20,
+  },
+});
