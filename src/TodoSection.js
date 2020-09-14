b89b4529d64dff82f5ac6cce67f7c8b0041c5e87
raunakchopra <raunakchopra@github.com>
2020-09-14 21:33:17 +0800

    Checked, Edit TODO functionality left

--- src/TodoSection.js
+++ src/TodoSection.js
@@ -8,6 +8,7 @@ import {
   IconButton,
 } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
+import EditIcon from "@material-ui/icons/Edit";
 
 function TodoSection(props) {
   const todos = props.todoData;
@@ -30,6 +31,9 @@ function TodoSection(props) {
                       }
                     />
                   </IconButton>
+                  {/* <IconButton>
+                    <EditIcon fontSize="large" className="editIcon" />
+                  </IconButton> */}
                 </Typography>
               }
               inset={true}
